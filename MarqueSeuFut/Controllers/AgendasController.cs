a71465a1be7de7fd429513682b4daede0cae6e0d
Rheydne Silva <rheydne@gmail.com>
2021-10-31 16:03:51 -0300

    alteracoes rey

---
+++ MarqueSeuFut/Controllers/AgendasController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MarqueSeuFut.Models;
+
+namespace MarqueSeuFut.Controllers
+{
+    public class AgendasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AgendasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Agenda
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Agendas.Include(a => a.TimeCasa).Include(a => a.TimeVisitante);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Agenda/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var agenda = await _context.Agendas
+                .Include(a => a.TimeCasa)
+                .Include(a => a.TimeVisitante)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (agenda == null)
+            {
+                return NotFound();
+            }
+
+            return View(agenda);
+        }
+
+        // GET: Agenda/Create
+        public IActionResult Create()
+        {
+            ViewData["TimeCasaId"] = new SelectList(_context.Times, "Id", "Nome");
+            ViewData["TimeVisitanteId"] = new SelectList(_context.Times, "Id", "Nome");
+            return View();
+        }
+
+        // POST: Agenda/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,DataHora,Localizacao,TimeCasaId,TimeVisitanteId")] Agenda agenda)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(agenda);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TimeCasaId"] = new SelectList(_context.Times, "Id", "Nome", agenda.TimeCasaId);
+            ViewData["TimeVisitanteId"] = new SelectList(_context.Times, "Id", "Nome", agenda.TimeVisitanteId);
+            return View(agenda);
+        }
+
+        // GET: Agenda/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var agenda = await _context.Agendas.FindAsync(id);
+            if (agenda == null)
+            {
+                return NotFound();
+            }
+            ViewData["TimeCasaId"] = new SelectList(_context.Times, "Id", "Nome", agenda.TimeCasaId);
+            ViewData["TimeVisitanteId"] = new SelectList(_context.Times, "Id", "Nome", agenda.TimeVisitanteId);
+            return View(agenda);
+        }
+
+        // POST: Agenda/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,DataHora,Localizacao,TimeCasaId,TimeVisitanteId")] Agenda agenda)
+        {
+            if (id != agenda.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(agenda);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AgendaExists(agenda.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TimeCasaId"] = new SelectList(_context.Times, "Id", "Nome", agenda.TimeCasaId);
+            ViewData["TimeVisitanteId"] = new SelectList(_context.Times, "Id", "Nome", agenda.TimeVisitanteId);
+            return View(agenda);
+        }
+
+        // GET: Agenda/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var agenda = await _context.Agendas
+                .Include(a => a.TimeCasa)
+                .Include(a => a.TimeVisitante)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (agenda == null)
+            {
+                return NotFound();
+            }
+
+            return View(agenda);
+        }
+
+        // POST: Agenda/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var agenda = await _context.Agendas.FindAsync(id);
+            _context.Agendas.Remove(agenda);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AgendaExists(int id)
+        {
+            return _context.Agendas.Any(e => e.Id == id);
+        }
+    }
+}
